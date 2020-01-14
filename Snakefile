9ef2abf22d4f510751ce143f441204a6a843a0a1
jsadler2 <jsadler@usgs.gov>
2020-01-14 16:47:57 -0600

    retrieves and combines metadata

--- Snakefile
+++ Snakefile
@@ -1,25 +1,48 @@
 import catch_attr
 
 configfile: 'catchment_attr_links.yaml'
-cat_att_dir = 'cat_attr/'
-cat_attr_out = "cat_attr/GeospatialFabricAttributes-PRMS_{cat}_02.gdb"
+regions = ['02']
+cat_att_dir = 'cat_attr'
 
 rule all:
     input:
-        f"{cat_att_dir}combined_categories.feather"
+        combined_data = expand("{fldr}/combined_categories_{region}.feather",
+                               fldr=cat_att_dir, region=regions),
+        metadata = f"{cat_att_dir}/combined_metadata.csv"
 
 
-rule get_all_cat_params:
+params_out_fmt = "{fldr}/GeospatialFabricAttributes-PRMS_{category}_{region}.gdb"
+rule get_all_catchment_params:
     output:
-        directory(expand(cat_attr_out, cat=config['categories']))
+        directory(params_out_fmt)
     run:
-        catch_attr.download_unzip_cat_attr_data(cat_att_dir, wildcards.cat)
+        catch_attr.download_unzip_cat_attr_data(wildcards.fldr,
+                                                wildcards.category,
+                                                wildcards.region)
 
 rule combine_categories:
     input:
         'catchment_attr_links.yaml',
-        rules.get_all_cat_params.output
+        expand(params_out_fmt, fldr=cat_att_dir, category=config['categories'],
+               region=regions)
     output:
-        f"{cat_att_dir}combined_categories.feather"
+        rules.all.input[0]
     run:
         catch_attr.combine_categories(input[1:], output[0])
+
+metadata_file_fmt = "{fldr}/{category}_metadata.xml"
+rule get_metadata_xml_files:
+    output:
+        metadata_file_fmt
+    run:
+        catch_attr.get_metadata_file(wildcards.category, output[0])
+
+rule combine_metadata_files:
+    input:
+        metadata_files = expand(metadata_file_fmt, fldr=cat_att_dir,
+               category=config['categories'])
+    output:
+        rules.all.input.metadata
+    run:
+        catch_attr.consolidate_metdata(input, output[0])
+
--- catch_attr.py
+++ catch_attr.py
@@ -1,4 +1,7 @@
+from bs4 import BeautifulSoup
 import os
+import json
+import requests
 import yaml
 import zipfile
 import pandas as pd
@@ -24,17 +27,21 @@ def retrieve_cat_attr_links(just_categories=False):
         return links['urls']
 
 
-def download_unzip_cat_attr_data(directory, category):
+def download_unzip_cat_attr_data(directory, category, region):
     """
     download files from the urls in the yaml file and then unzip the downloaded
     data
     :param directory: [str] path to the directory where the catchment attribute
     data should be downloaded and unzipped to
+    :param region: [str] zero-padded number for the region (e.g., '02')
+    :param category: [str] the category of parameter (SSURGO, NLCD, etc.)
     :return: [list] list of file locations of the downloaded and unzipped data
     """
-    links = retrieve_cat_attr_links()
-    zipped_path = directory + category
-    urllib.request.urlretrieve(links[category], zipped_path)
+    print(directory, category, region)
+    zipped_path = directory+'/' + category + region
+    download_url = retrieve_file_url(category, region)
+    print (download_url)
+    urllib.request.urlretrieve(download_url, zipped_path)
     with zipfile.ZipFile(zipped_path, 'r') as zip_ref:
         zip_ref.extractall(directory)
     os.remove(zipped_path)
@@ -54,10 +61,90 @@ def combine_categories(cat_data_files, output_file):
         df = pd.DataFrame(gdf.drop(columns='geometry'))
         df.set_index('hru_id', inplace=True)
         df_list.append(df)
-    df_combined = pd.concat(df_list)
+    df_combined = pd.concat(df_list, axis=1)
+
+    # remove any duplicated columns
+    df_combined = df_combined.loc[:, ~df_combined.columns.duplicated()]
 
     # save to feather
     df_combined.reset_index(inplace=True)
     df_combined.to_feather(output_file)
     return df_combined
+
+
+def read_sb_json(sb_url):
+    """
+    read the json data from a sciencbase resource into a dictionary
+    :param sb_url: [str] url of the sciencbase resource
+    """
+    response = requests.get(sb_url)
+    return json.loads(response.text)
+
+
+def retrieve_category_json(category):
+    """
+    form the url and get the json data for a certain catchment attribute
+    category
+    """
+    url = retrieve_cat_attr_links()[category]
+    # make sure we are getting the data back as json
+    url += "?format=json"
+    data = read_sb_json(url)
+    return data
+
+
+def retrieve_file_url(category, region):
+    """
+    retrieve the data download url from the science base entry
+    :param category: [str] the category of parameter (SSURGO, NLCD, etc.)
+    :param region: [str] zero-padded number for the region (e.g., '02')
+    """
+    category_data = retrieve_category_json(category)
+    for file_data in category_data['files']:
+        should_end_with = f"{category}_{region}.zip".lower()
+        if file_data['name'].lower().endswith(should_end_with):
+           return file_data['downloadUri']
+
+
+def get_metadata_file(category, output_file):
+    """
+    download the metadata json file for a given category
+    :param category: [str] the category of parameter (SSURGO, NLCD, etc.)
+    :param output_file: [str] the output file path where the metadata should
+    be saved
+    """
+    category_data = retrieve_category_json(category)
+    for file_data in category_data['files']:
+        if file_data['originalMetadata']:
+            urllib.request.urlretrieve(file_data['downloadUri'], output_file)
+
+
+def parse_metadata_file(file_path):
+    with open(file_path, 'r') as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+    data = []
+    for attr in soup.find_all('attr'):
+        data_dict = {}
+        if attr.attrlabl.text != 'hru_id':
+            data_dict['attribute_label'] = attr.attrlabl.text
+            data_dict['attribute_definition'] = attr.attrdef.text
+            try:
+                data_dict['attribute_units'] = attr.attrunit.text
+            except AttributeError:
+                data_dict['attribute_units'] = 'NA'
+        data.append(data_dict)
+    return data
+    
+
+def consolidate_metdata(metadata_files, output_file):
+    combined_data_list = []
+    for metadata_file in metadata_files:
+        category_data_dict = parse_metadata_file(metadata_file)
+        combined_data_list.extend(category_data_dict)
+    df = pd.DataFrame(combined_data_list)
+    df.dropna(inplace=True)
+    df.to_csv(output_file, index=False)
+    return df
+
 
