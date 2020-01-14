3d555fe7f254a18cfdabd7a533bef9722e2c0bcd
jsadler2 <jsadler@usgs.gov>
2020-01-14 13:05:56 -0600

    combining all attr

--- Snakefile
+++ Snakefile
@@ -6,10 +6,20 @@ cat_attr_out = "cat_attr/GeospatialFabricAttributes-PRMS_{cat}_02.gdb"
 
 rule all:
     input:
-        expand(cat_attr_out, cat=config['categories'])
+        f"{cat_att_dir}combined_categories.feather"
+
 
 rule get_all_cat_params:
     output:
-        directory(cat_attr_out)
+        directory(expand(cat_attr_out, cat=config['categories']))
     run:
         catch_attr.download_unzip_cat_attr_data(cat_att_dir, wildcards.cat)
+
+rule combine_categories:
+    input:
+        'catchment_attr_links.yaml',
+        rules.get_all_cat_params.output
+    output:
+        f"{cat_att_dir}combined_categories.feather"
+    run:
+        catch_attr.combine_categories(input[1:], output[0])
--- catch_attr.py
+++ catch_attr.py
@@ -1,21 +1,28 @@
 import os
 import yaml
 import zipfile
-# import pandas as pd
-# import geopandas as gpd
+import pandas as pd
+import geopandas as gpd
 import urllib
 
-def retrieve_cat_attr_links():
+
+def retrieve_cat_attr_links(just_categories=False):
     """
     retrieve a list of links that are for catchment attributes. the links are 
     stored in a yaml file called catchment_attr_links.yaml
+    :param just_categories: [bool] whether or not to return just the categories
+    or the categories with the urls too
     return: [dict] a dictionary where the keys are the categories of attributes
     and the values are the urls for retrieving the data
     """
     link_file = "catchment_attr_links.yaml"
     with open(link_file, 'r') as f:
         links = yaml.safe_load(f)
-    return links['categories']
+    if just_categories:
+        return links['categories']
+    else:
+        return links['urls']
+
 
 def download_unzip_cat_attr_data(directory, category):
     """
@@ -31,3 +38,26 @@ def download_unzip_cat_attr_data(directory, category):
     with zipfile.ZipFile(zipped_path, 'r') as zip_ref:
         zip_ref.extractall(directory)
     os.remove(zipped_path)
+
+
+def combine_categories(cat_data_files, output_file):
+    """
+    read in and combine all of the data from all catchment attribute category
+    files (SSURGO, NLCD, etc.)
+    :param cat_data_files: [list] list of paths where the data files are
+    :param output_file: [str] path to where the combined file should be written
+    """
+    # combine all the files
+    df_list = []
+    for data_file in cat_data_files:
+        gdf = gpd.read_file(data_file)
+        df = pd.DataFrame(gdf.drop(columns='geometry'))
+        df.set_index('hru_id', inplace=True)
+        df_list.append(df)
+    df_combined = pd.concat(df_list)
+
+    # save to feather
+    df_combined.reset_index(inplace=True)
+    df_combined.to_feather(output_file)
+    return df_combined
+
