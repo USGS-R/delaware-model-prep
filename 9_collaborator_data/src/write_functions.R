3db0b47b21f8d0012c7fbc9ab865ee6647b8a629
Sam Oliver <soliver@usgs.gov>
2020-01-28 11:13:56 -0600

    add depends and the observation data (full and subset)

---
+++ 9_collaborator_data/src/write_functions.R
@@ -0,0 +1,38 @@
+write_to_csv <- function(dat_ind, out_ind) {
+  dat <- readRDS(sc_retrieve(dat_ind))
+  readr::write_csv(dat, as_data_file(out_ind))
+  gd_put(out_ind)
+}
+
+filter_subset <- function(dat_ind, subnet_ind, out_ind) {
+  dat <- readRDS(sc_retrieve(dat_ind))
+  subnet <- readRDS(sc_retrieve(subnet_ind))
+  
+  subnet_dat <- filter(dat, seg_id_nat %in% unique(subnet$edges$seg_id_nat))
+  
+  readr::write_csv(subnet_dat, as_data_file(out_ind))
+  gd_put(out_ind)
+}
+
+# save distance matrices in numpy format
+save_dist_matrices <- function(dist_mat_ind, out_ind) {
+  np <- reticulate::import('numpy')
+  
+  dist_mat_list <- readRDS(sc_retrieve(dist_mat_ind))
+  
+  out_file <- as_data_file(out_ind)
+  np$savez_compressed(
+    file=out_file,
+    downstream=dist_mat_list$downstream,
+    upstream=dist_mat_list$upstream,
+    complete=dist_mat_list$complete,
+    updown=dist_mat_list$updown)
+  # R access examples:
+  # loaded <- np$load('out/dists.npz')
+  # loaded$files
+  # loaded$f[['updown']]
+  
+  gd_put(out_ind)
+  
+  
+}
