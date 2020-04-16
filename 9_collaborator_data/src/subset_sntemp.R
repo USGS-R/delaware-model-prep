59b9ee2aa87271bc6ff3b56df788fd393feeac37
Alison Appling <aappling@usgs.gov>
2020-04-16 17:06:53 -0400

    revise sntemp output aggregations

--- 9_collaborator_data/src/subset_sntemp.R
+++ 9_collaborator_data/src/subset_sntemp.R
@@ -79,26 +79,30 @@ aggregate_sntemp_preds = function(ind_file,
       # "table" refers to https://water.usgs.gov/water-resources/software/PRMS/PRMS_tables_5.0.0.pdf.
       summarize(
         # drivers
-        basin_ccov = weighted.mean(seg_ccov, hrus_area_km2), # fraction, but not in table; area-weighted average cloud cover fraction for each segment from HRUs contributing flow to the segment
-        basin_humid = weighted.mean(seg_humid, hrus_area_km2), # fraction (0-1), in table as "humidity"; area-weighted average relative humidity for each segment from HRUs contributing flow to the segment
-        basin_rain = weighted.mean(seg_rain, hrus_area_km2), # not in table, but basin_rain and hru_rain are in inches; area-weighted average rainfall for each segment from HRUs contributing flow to the segment
-        basin_shade = weighted.mean(seg_shade, hrus_area_km2), # seems to be always 0. area-weighted average shade fraction for each segment
-        basin_tave_air = weighted.mean(seg_tave_air, hrus_area_km2), # area-weighted air temperature for each segment from HRUs contributing flow to the segment
-        basin_gwflow = weighted.mean(seginc_gwflow, hrus_area_km2), # cfs; area-weighted average groundwater discharge for each segment from HRUs contributing flow to the segment
-        basin_potet = weighted.mean(seginc_potet, hrus_area_km2), # inches; area-weighted average potential ET for each segment from HRUs contributing flow to the segment
-        basin_sroff = weighted.mean(seginc_sroff, hrus_area_km2), # cfs; area-weighted average [SUM??] surface runoff for each segment from HRUs contributing flow to the segment
-        basin_ssflow = weighted.mean(seginc_ssflow, hrus_area_km2), # cfs; area-weighted average [SUM??] interflow for each segment from HRUs contributing flow to the segment
-        basin_swrad = weighted.mean(seginc_swrad, hrus_area_km2), # langleys or W m^-2?; area-weighted average solar radiation for each segment from HRUs contributing flow to the segment
+        basin_ccov_spd = sum(seg_ccov * hrus_area_km2), # fraction, but not in table; area-weighted average cloud cover fraction for each segment from HRUs contributing flow to the segment
+        basin_humid_awm = weighted.mean(seg_humid, hrus_area_km2), # fraction (0-1), in table as "humidity"; area-weighted average relative humidity for each segment from HRUs contributing flow to the segment
+        basin_rain_awm = weighted.mean(seg_rain, hrus_area_km2), # not in table, but basin_rain and hru_rain are in inches; area-weighted average rainfall for each segment from HRUs contributing flow to the segment
+        basin_shade_lwm = weighted.mean(seg_shade, seg_length_km), # seems to be always 0. area-weighted average shade fraction for each segment
+        basin_tave_air_awm = weighted.mean(seg_tave_air, hrus_area_km2), # area-weighted air temperature for each segment from HRUs contributing flow to the segment
+        basin_potet_awm = weighted.mean(seginc_potet, hrus_area_km2), # inches; area-weighted average potential ET for each segment from HRUs contributing flow to the segment
+        basin_swrad_awm = weighted.mean(seginc_swrad, hrus_area_km2), # langleys or W m^-2?; area-weighted average solar radiation for each segment from HRUs contributing flow to the segment
+        # cfs drivers (we think sum is better than awm for these variables, but farshid asked to keep both)
+        basin_gwflow_awm = weighted.mean(seginc_gwflow, hrus_area_km2), # cfs; area-weighted average groundwater discharge for each segment from HRUs contributing flow to the segment
+        basin_gwflow_sum = sum(seginc_gwflow), # cfs; area-weighted average groundwater discharge for each segment from HRUs contributing flow to the segment
+        basin_sroff_awm = weighted.mean(seginc_sroff, hrus_area_km2), # cfs; area-weighted average [SUM??] surface runoff for each segment from HRUs contributing flow to the segment
+        basin_sroff_sum = sum(seginc_sroff), # cfs; area-weighted average [SUM??] surface runoff for each segment from HRUs contributing flow to the segment
+        basin_ssflow_awm = weighted.mean(seginc_ssflow, hrus_area_km2), # cfs; area-weighted average [SUM??] interflow for each segment from HRUs contributing flow to the segment
+        basin_ssflow_sum = sum(seginc_ssflow), # cfs; area-weighted average [SUM??] interflow for each segment from HRUs contributing flow to the segment
         # values we probably won't use as predictors (too internal to the temperature module):
-        basin_tave_gw = weighted.mean(seg_tave_gw, hrus_area_km2), # assuming C; groundwater temperature
-        basin_tave_sroff = weighted.mean(seg_tave_sroff, hrus_area_km2), # assuming C; surface runoff temperature
-        basin_tave_ss = weighted.mean(seg_tave_ss, hrus_area_km2), # assuming C; subsurface temperature
+        basin_tave_gw_awm = weighted.mean(seg_tave_gw, hrus_area_km2), # assuming C; groundwater temperature
+        basin_tave_sroff_awm = weighted.mean(seg_tave_sroff, hrus_area_km2), # assuming C; surface runoff temperature
+        basin_tave_ss_awm = weighted.mean(seg_tave_ss, hrus_area_km2), # assuming C; subsurface temperature
         # basin properties
-        basin_area = sum(hrus_area_km2),
-        network_slope = weighted.mean(seg_slope, seg_length_km), # (not in table. hru_slope is decimal fraction, 0 to 10); Slope of segments
-        network_length = sum(seg_length_km), # km; Length of each segment
-        network_width = weighted.mean(seg_width, seg_length_km), # m, width of each segment
-        network_elev = weighted.mean(seg_elev, seg_length_km), # m?; Elevation of each segment, used to estimate atmospheric pressure
+        basin_area_sum = sum(hrus_area_km2),
+        network_slope_lwm = weighted.mean(seg_slope, seg_length_km), # (not in table. hru_slope is decimal fraction, 0 to 10); Slope of segments
+        network_length_sum = sum(seg_length_km), # km; Length of each segment
+        network_width_lwm = weighted.mean(seg_width, seg_length_km), # m, width of each segment
+        network_elev_lwm = weighted.mean(seg_elev, seg_length_km), # m?; Elevation of each segment, used to estimate atmospheric pressure
         # outlet properties (these are actually properties of the most downstream segment in the network - close enough to consider "outlet", I hope)
         outlet_slope = seg_slope[is_outlet], # m, width of each segment
         outlet_width = seg_width[is_outlet], # m, width of each segment
@@ -116,40 +120,4 @@ aggregate_sntemp_preds = function(ind_file,
   feather::write_feather(x = out, path = out_file)
   gd_put(ind_file)
 }
-
-# Inspect the outputs to make sure the ranges are reasonable
-# raw_preds <- feather::read_feather('9_collaborator_data/psu/sntemp_preds_high_obs_sites.feather')
-# agg_preds <- feather::read_feather('9_collaborator_data/psu/sntemp_preds_aggregated.feather')
-# # basin_ccov seems fine, ranges from 0.25 to 0.8
-# raw_preds$seg_ccov %>% summary
-# agg_preds$basin_ccov %>% summary
-# ggplot(agg_preds, aes(x=date, y=basin_ccov, color=outlet_seg_id_nat)) + geom_line()
-# # CHECK AFTER REBUILD basin_humid is boring, always 0.7
-# raw_preds$seg_humid %>% summary
-# agg_preds$basin_humid %>% summary
-# ggplot(agg_preds, aes(x=date, y=basin_humid, color=outlet_seg_id_nat)) + geom_line()
-# # basin_rain
-# raw_preds$seg_rain %>% summary
-# agg_preds$basin_rain %>% summary
-# ggplot(agg_preds, aes(x=date, y=basin_humid, color=outlet_seg_id_nat)) + geom_line() + facet_wrap(~ outlet_seg_id_nat)
-# # basin_shade
-# # basin_tave_air
-# # basin_gwflow
-# # basin_potet
-# # basin_sroff
-# # basin_ssflow
-# # basin_swrad
-# # basin_tave_gw
-# # basin_tave_sroff
-# # basin_tave_ss
-# # basin_area
-# # network_slope
-# # network_length
-# # network_width
-# # network_elev
-# # outlet_slope
-# # outlet_width
-# # outlet_elev
-# # outlet_tave_water
-# # outlet_outflow
 # # outlet_seg_id_nat
