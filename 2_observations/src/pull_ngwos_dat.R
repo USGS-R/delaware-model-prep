f1adc549df9dbb3ece7aa8495bc9b190b82b1b12
Samantha Oliver <soliver@usgs.gov>
2020-05-01 14:42:31 -0500

    remove old NGWOS site munging code, modify summarize to ID sites to keep.

--- 2_observations/src/pull_ngwos_dat.R
+++ 2_observations/src/pull_ngwos_dat.R
@@ -1,16 +1,6 @@
 retrieve_ngwos <- function(out_ind, sites_ind) {
   # get latest NGWOS data
   
-  # these sites painstakingly retrieved from 
-  # https://www.usgs.gov/mission-areas/water-resources/science/next-generation-water-observing-system-delaware-river-basin?qt-science_center_objects=1#qt-science_center_objects
-  # note I only kept enhanced gages that mentioned temperature, which was confirmed by looking at the others which have
-  # temperature data that predates NGWOS
-  new_ngwos <-  c('01455300', '01464040', '01464598', '01483050', '01416900', '01419500', '01424997', '01427500', 
-                  '01428000', '01435800', '01427200', '01432500', '01467200', '01471875', '01475510')
-  
-  enhanced_ngwos <- c('01478000', '01478650', '01479000', '01480000', '01438500', '01446500', '01457500', 
-                      '01462000', '01413500', '01423000', '01427207', '01432160', '01434000', '01437500', 
-                      '01431500', '01465500', '01467087', '01473000', '01474500', '01470500')
   
   ngwos_meta <- readxl::read_xlsx(sc_retrieve(sites_ind))
   
@@ -107,44 +97,7 @@ retrieve_ngwos <- function(out_ind, sites_ind) {
   ## return NGWOS data
   saveRDS(all_dat, as_data_file(out_ind))
   gd_put(out_ind)
-  
-  
-  ##################
-  # old way
-  
-  # new_ngwos_dat <- dataRetrieval::readNWISdv(siteNumbers = new_ngwos, parameterCd = '00010') 
-  # 
-  # 
-  # 
-  # length(unique(new_ngwos_dat$site_no))
-  # 
-  # length(unique(new_ngwos_dat$site_id))
-  # enhanced_dat <- dataRetrieval::readNWISdv(siteNumbers = enhanced_ngwos, parameterCd = '00010') %>%
-  #   mutate(site_id = paste0('USGS-', site_no), 
-  #          source = 'nwis_dv',
-  #          temp_degC = ifelse(is.na(X_00010_00003), `X_..2.._00010_00003`, X_00010_00003),
-  #          n_obs = 1) %>%
-  #   select(site_id, date = Date, temp_degC, source)
-  # 
-  # length(unique(enhanced_dat$site_id))
-  # 
-  # # check for missing sites on UV
-  # 
-  # 
-  # missing_sites <- c(new_ngwos[!new_ngwos %in% gsub('USGS-', '', unique(new_ngwos_dat$site_id))], 
-  #                    enhanced_ngwos[!enhanced_ngwos %in% gsub('USGS-', '', unique(enhanced_dat$site_id))])
-  # 
-  # ngwos_uv <- dataRetrieval::readNWISuv(siteNumbers = missing_sites, parameterCd = '00010') %>%
-  #   mutate(date = as.Date(dateTime), 
-  #          site_id = paste0('USGS-', site_no), source = 'nwis_uv') %>%
-  #   select(site_id, date, temp_degC = 'X_00010_00000', source) %>%
-  #   group_by(site_id, date, source) %>%
-  #   summarize(temp_degC = mean(temp_degC), n_obs = n())
-  # 
-  # all_dat <- bind_rows(ngwos_uv, new_ngwos_dat, enhanced_dat)
-  # 
-  # saveRDS(all_dat, as_data_file(out_ind))
-  # gd_put(out_ind)
+
 }
 
 # figure out which NGWOS sites to withhold
@@ -196,7 +149,8 @@ explore_ngwos <- function() {
   
   date_summary_all <- ngwos_dat %>%
     group_by(site_id) %>%
-    summarize(n_pre_NGWOS = length(date[date < as.Date('2017-10-01')]),
+    summarize(n_pre_NGWOS = length(date[date < as.Date('2017-10-01') & date > as.Date('2013-10-01')]),
+              n_pre_all = length(date[date < as.Date('2017-10-01')]),
               n_post_NGWOS = length(date[date >= as.Date('2017-10-01')]))
   
   
@@ -209,6 +163,13 @@ explore_ngwos <- function() {
   
   # sites I could not find
   temp_sites$site_id[which(!temp_sites$site_id %in% unique(ngwos_dat$site_id))]
+  
+  # are the ngwos data in the filtered sites? 
+  # first, are they in the site list at all? 
+  
+  ngwos_sites <- unique(ngwos_dat$site_id)
+  all_sites <- readRDS('2_observations/out/crosswalk_site_reach.rds')
+  summary(ngwos_sites %in% gsub('USGS-', '', unique(all_sites$site_id)))
 }
 
 
