2d1ef0c9d8b50052614b97e886e19ee7f2d25c39
Atshan <ratshan@usgs.gov>
2020-08-10 16:45:32 -0500

    Renamed file and needed to change width on line 36 from "3" to "6".

--- 8_visualize/src/plot_metric_map .R
+++ 8_visualize/src/plot_metric_map .R
@@ -1,8 +1,8 @@
 
-plot_metric_map <- function(network_ind, metric_file, metric_col, plot_model, out_file) {
+plot_metric_map <- function(out_file, network_ind, metric_file, metric_col, plot_model, legend_text) {
   #finding the network that includes the spatial information  "geometries" for plotting
   network <- readRDS(sc_retrieve(network_ind, 'getters.yml'))
-  network <- network$edges %>% mutate(seg_id_nat = as.character(seg_id_nat))
+  network <- network$edges
   # reading the metric csv file
   dat <- read_csv(file = metric_file)
 
@@ -20,18 +20,18 @@ plot_metric_map <- function(network_ind, metric_file, metric_col, plot_model, ou
     plot.margin = margin(0,0,0,0)
   )
   # filtering to select the full models.
-  model_col <- dat %>%
+  model_dat <- dat %>%
     filter(model %in% plot_model)
   #combine the metrics measurement with the network
-  metric_network <- left_join(network, model_col)
+  metric_network <- left_join(network, model_dat)
   # plotting the metric associated with the full models.
-  browser()
+  #browser()
   p <- ggplot(metric_network) +
-    geom_sf(aes(color = metric_col), size = 1.2) +
-    scale_color_viridis_c(direction = -1, option = 'inferno', na.value = 'lightgray',  limits = c(0, 9)) +
+    geom_sf(aes(color = .data[[metric_col]]), size = 1.2) +
+    scale_color_viridis_c(direction = -1, option = 'inferno', na.value = 'lightgray') +
     theme_bw() +
     map_theme +
-    labs(color = {{metric_col}})
+    labs(color = legend_text)
 
-  ggsave(out_file, p, height = 6, width = 3)
+  ggsave(out_file, p, height = 6, width = 6)
 }
