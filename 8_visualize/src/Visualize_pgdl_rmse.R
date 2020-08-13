6dd5fc4bf7215123362c231e18dade1ed81b47f9
Atshan <ratshan>
2020-08-13 11:09:17 -0500

    Lines (46-54): trying to add the legend labels (e.g. "+ pretraining". and x-axis values (e.g. 2%)

--- 8_visualize/src/Visualize_pgdl_rmse.R
+++ 8_visualize/src/Visualize_pgdl_rmse.R
@@ -9,14 +9,14 @@ library(dplyr)
 library(tidyverse)
 library(shades)
 #process guidance: summer and non-summer rmse table
-non_summer_train <- data.frame(matrix(c('plain neural network', 'time awareness', 'pretraining', 'space awareness', 1.5, 1.3, 1.3, 1.3, 2.1, 2.1, 1.9, 1.7), nrow = 4, ncol = 3))
+non_summer_train <- data.frame(matrix(c('plain_neural_network', 'time_awareness', 'pretraining', 'space_awareness', 1.5, 1.3, 1.3, 1.3, 2.1, 2.1, 1.9, 1.7), nrow = 4, ncol = 3))
 colnames(non_summer_train)[1] = 'model'
 colnames(non_summer_train)[2] = 'Non_summer'
 colnames(non_summer_train)[3] = 'Summer'
-# writing the dataframe ti csv file
+# writing the dataframe to csv file
 non_summer_train_dat <-readr::write_csv(non_summer_train, path = '8_visualize/in/non_summer_train_dat.csv')
 # less need of data: rmse on test periods table.
-subset_train <- data.frame(matrix(c('Uncalibrated process model', 'plain neural network', 'time awareness', 'pretraining', 'space awareness', 3.7, 1.6, 1.5, 1.44, 1.40, 3.7, 1.8, 1.7, 1.5, 1.43, 3.7, 2.2, 1.9, 1.8, 1.6), nrow = 5, ncol = 4))
+subset_train <- data.frame(matrix(c('Uncalibrated_process_model', 'plain_neural_network', 'time_awareness', 'pretraining', 'space_awareness', 3.7, 1.6, 1.5, 1.44, 1.40, 3.7, 1.8, 1.7, 1.5, 1.43, 3.7, 2.2, 1.9, 1.8, 1.6), nrow = 5, ncol = 4))
 colnames(subset_train)[1] = 'model'
 colnames(subset_train)[2] = '100'
 colnames(subset_train)[3] = '10'
@@ -25,32 +25,44 @@ subset_train_dat <- readr::write_csv(subset_train, path = '8_visualize/in/subset
 
 #reading the data csv files and using pivot_long to change the data from wide to long.
 non_summer_mod_dat <- readr::read_csv('8_visualize/in/non_summer_train_dat.csv') %>%
-  pivot_longer(c(-model), names_to = 'test_period', values_to = 'rmse')
+  pivot_longer(c(-model), names_to = 'season', values_to = 'rmse')
 
 subset_mod_dat <- readr::read_csv('8_visualize/in/subset_train_dat.csv') %>%
-  filter(!model %in%  'Uncalibrated process model') %>%
+  filter(!model %in%  'Uncalibrated_process_model') %>%
   pivot_longer(c(-model), names_to = 'dat_availability', values_to = 'rmse')
 subset_mod_dat$dat_availability <- factor(subset_mod_dat$dat_availability, levels = c('2', '10', '100'))
-subset_mod_dat$model <- factor(subset_mod_dat$model, levels = c('space awareness', 'pretraining', 'time awareness','plain neural network'))
+subset_mod_dat$model <- factor(subset_mod_dat$model, levels = c('space_awareness', 'pretraining', 'time_awareness','plain_neural_network'))
 
 
 #  line plot for `non-summer_mod_dat` with the 1, 10, 100% on the x, model metric on the y, and each model getting their own line.
 color <- c('#47437e', '#7570b3', '#b3b0d5', '#d95f02')
 plot_label <- c('+ space awareness', '+ pretraining', '+ time awareness','plain neural network')
-  ggplot(data = subset_mod_dat, aes(x = dat_availability, y = rmse, group =     model, colour = model)) +
-   geom_line() +
-   geom_point(aes(shape = model))  +
-   #scale_x_continuous(labels = c("2%", "10%", "100%")) +
-   scale_y_continuous(trans = "reverse") +
-   theme_bw()+
-   theme(legend.title = element_blank())+
-   scale_color_manual(values = color) +
-    scale_shape_manual(labels= plot_label) +
-    labs(x = 'Training Used Data',
-         y = 'RMSE')
+  ggplot(data = subset_mod_dat, aes(x = dat_availability,
+                                    y = rmse,
+                                    group = model,
+                                    colour = model)) +
+     geom_line() +
+     geom_point(aes(shape = model))  +
+     #scale_shape_manual(labels= c(space_awareness =
+                          #           '+ space awareness',
+                           #       pretraining =
+                            #         '+ pretraining',
+                             #     time_awareness =
+                              #       '+ time awareness',
+                               #   plain_neural_network =
+                                #     'plain neural network')) +
+     #scale_x_continuous(label = c("2%", "10%", "100%")) +
+     scale_y_continuous(trans = "reverse") +
+     theme_bw()+
+     theme(legend.title = element_blank())+
+     scale_color_manual(values = color) +
+     labs(x = 'Training Used Data',
+          y = 'RMSE')
 
-
-
+#seasonal test as a bar plot.Probably color = model or color = season, with whatever is not used for color as the primary grouping variable for the bars (e.g., if color = season, bars should be grouped by model)
+ggplot(data = non_summer_mod_dat, aes(x = rmse, colour = model, group = season)) +
+   geom_col(stat = "count", size = 1)
+class(non_summer_mod_dat$model)
 
 
 
