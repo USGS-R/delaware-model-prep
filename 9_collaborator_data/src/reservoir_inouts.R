efe9894f658278762e04d9deea999d7ea1ac3bf5
Alison Appling <aappling@usgs.gov>
2021-03-05 16:04:28 -0500

    move res code to phase 9

--- 9_collaborator_data/src/reservoir_inouts.R
+++ 9_collaborator_data/src/reservoir_inouts.R
@@ -3,29 +3,97 @@ library(tidyverse)
 
 #### Observations ####
 
+#' @param res_outflow_ids named list where each element is a vector of 1 or more NWIS site IDs just downstream of the reservoir (above which any stream sites can be assumed to be tributaries)
+#' @param flow_indpath of the .ind file of the smallest, most munged file available that still has all the discharge observation sites in DRB (or beyond)
+#' @param temp_ind path of the .ind file of the smallest, most munged file available that still has all the temperature observation sites in DRB
+#' @param min_obs the minimum number of observations (of flow or temperature) that a site must have before we'll consider it
+find_inout_obs_sites <- function(
+  res_outflow_ids = list(Cannonsville = '01425000', Pepacton = c('01417000','01417500')),
+  flow_ind = '2_observations/in/daily_flow.rds.ind', # '2_observations/out/drb_discharge_daily_dv.csv.ind',
+  temp_ind = '2_observations/out/all_drb_temp_obs.rds.ind', # '2_observations/in/daily_temperatures.rds.ind',
+  min_obs_flow = 1000,
+  min_obs_temp = 50,
+  max_dist_km = 100) {
+
+  # read in the available data; do this early so we know which sites have data potential (i.e., at least 1000 obs)
+  #flow_dat <- readr::read_csv(sc_retrieve(flow_ind, 'getters.yml'), col_types=cols(datetime='D', .default='d'))
+  flow_dat <- sc_retrieve(flow_ind, 'getters.yml') %>% read_rds()
+  temp_dat <- sc_retrieve(temp_ind, 'getters.yml') %>% read_rds()
+  flow_sites <- flow_dat %>% group_by(site_id) %>% tally() %>% filter(n >= min_obs_flow) %>% pull(site_id) %>% unique()
+  temp_sites <- temp_dat %>% group_by(site_id) %>% tally() %>% filter(n >= min_obs_temp & grepl('^USGS-', site_id)) %>%
+    mutate(site_id = gsub('USGS-', '', site_id)) %>% pull(site_id) %>% unique()
+  obs_sites <- list('flow'=flow_sites, 'temp'=temp_sites)
+
+  # Use NLDI to algorithmically pick out inflows. Choose those sites that meet the
+  # min_obs criterion above and are closest to the reservoir along a given upstream
+  # route (i.e., don't choose two sites on the same inflow tributary).
+  # NLDI: http://usgs-r.github.io/dataRetrieval/reference/findNLDI.html.
+  # As of 2/2/21, dataRetrieval access to NLDI requires
+  # remotes::install_gitlab('water/dataRetrieval', host='code.usgs.gov', auth_token='your_code.usgs.gov_token')
+  res_inflow_ids <- purrr::map(setNames(nm=c('flow','temp')), function(variable) {
+    purrr::map(res_outflow_ids, function(outflow_ids) {
+
+      # find all reasonable candidate sites within the specified maximum river distance from the outlet
+      upstream_ids <- purrr::map(outflow_ids, function(outflow_id) {
+        upstream_ids_list <- dataRetrieval::findNLDI(
+          nwis = outflow_id, find='nwissite', nav='UT', distance_km = max_dist_km, no_sf=TRUE)
+        setdiff(upstream_ids_list$UT_nwissite$identifier, upstream_ids_list$origin$identifier) %>%
+          intersect(obs_sites[[variable]])
+      }) %>% unlist() %>% unique() %>% setdiff(outflow_ids)
+      message(sprintf('### UPSTREAM OF %s: %s', paste(outflow_ids, collapse=' & '), paste(sort(upstream_ids), collapse=', ')))
+      if(length(upstream_ids) == 0) message('  found no data-rich upstream sites')
+
+      first_upstream <- purrr::map_lgl(upstream_ids, function(upstream_id) {
+        downstream_ids_list <- dataRetrieval::findNLDI(
+          nwis = upstream_id, find='nwissite', nav='DM', distance_km = max_dist_km)
+        downstream_ids <- setdiff(downstream_ids_list$DM_nwissite$identifier, downstream_ids_list$origin$identifier)
+        # message(sprintf('%s is upstream of: %s', upstream_id, paste(sort(downstream_ids), collapse=', ')))
+        is_first_upstream <- !any(downstream_ids %in% upstream_ids)
+        message(sprintf('  %s: %s is directly upstream', is_first_upstream, upstream_id), appendLF = is_first_upstream)
+        if(!is_first_upstream) {
+          message(sprintf('; interrupted by %s', paste(downstream_ids[downstream_ids %in% upstream_ids], collapse=', ')))
+        }
+        return(is_first_upstream)
+      })
+      return(upstream_ids[first_upstream])
+    })
+  })
+}
+
+# inout_obs_sites <- list(
+#   flow = list(
+#     Cannonsville = c("01423500", "01423000", "01424500"),
+#     Pepacton = c("01413500", "01415000", "01416500", "01414000", "01414500")),
+#   temp = list(
+#     Cannonsville = "01423000",
+#     Pepacton = "01413088"))
+
 get_inout_obs <- function(
   site_ids = c(),
-  flow_ind = '2_observations/out/drb_discharge_daily_dv.csv.ind', # /out/, not /in/? But I don't see a recipe for it so assume it's from Jeff
-  temp_ind = '2_observations/in/daily_temperatures.rds.ind') {
+  flow_ind = '2_observations/in/daily_flow.rds.ind', # '2_observations/out/drb_discharge_daily_dv.csv.ind',
+  temp_ind = '2_observations/out/all_drb_temp_obs.rds.ind' # '2_observations/in/daily_temperatures.rds.ind',
+) {
 
   # read raw data
-  flow_raw <- sc_retrieve(flow_ind, 'getters.yml') %>%
-    readr::read_csv(col_types=cols(datetime='D', .default='d'))
-  temp_raw <- sc_retrieve(temp_ind, 'getters.yml') %>%
-    read_rds()
+  # flow_raw <- sc_retrieve('2_observations/out/drb_discharge_daily_dv.csv.ind', 'getters.yml') %>%
+  #   readr::read_csv(col_types=cols(datetime='D', .default='d'))
+  flow_raw <- sc_retrieve(flow_ind, 'getters.yml') %>% read_rds()
+  temp_raw <- sc_retrieve(temp_ind, 'getters.yml') %>% read_rds()
 
   # filter and format data for these sites
+  # flows <- flow_raw %>%
+  #   select(datetime, any_of(site_ids)) %>%
+  #   # is it CFS? or CMS, or other?
+  #   pivot_longer(-datetime, names_to='site_id', values_to='flow_cms') %>%
+  #   select(site_id, date = datetime, flow_cms) %>%
+  #   filter(!is.na(flow_cms))
   flows <- flow_raw %>%
-    select(datetime, any_of(site_ids)) %>%
-    # is it CFS? or CMS, or other?
-    pivot_longer(-datetime, names_to='site_id', values_to='flow_cms') %>%
-    select(site_id, date = datetime, flow_cms) %>%
-    filter(!is.na(flow_cms))
+    filter(site_id %in% site_ids) %>%
+    mutate(flow_cms = flow_cfs / 35.314666) %>%
+    select(-flow_cfs)
   temps <- temp_raw %>%
     filter(site_id %in% paste0('USGS-', site_ids)) %>%
-    mutate(site_id = gsub('USGS-', '', site_id)) %>%
-    select(site_id, date, temp_degC) %>%
-    filter(!is.na(temp_degC))
+    mutate(site_id = gsub('USGS-', '', site_id))
   site_dates <- crossing(
     site_id = site_ids,
     date = seq(
@@ -34,7 +102,7 @@ get_inout_obs <- function(
       by=as.difftime(1, units='days')))
 
   # merge flows and temps, adding NAs for unobserved dates within a complete date sequence
-  all_dat <- full_join(flows, temps, by=c('site_id','date')) %>%
+  all_dat <- full_join(flows, temps, by=c('site_id','date'), suffix=c('_flow','_temp')) %>%
     full_join(site_dates, by=c('site_id','date'))
 
   return(all_dat)
