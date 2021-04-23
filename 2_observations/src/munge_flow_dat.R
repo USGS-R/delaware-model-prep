e6abbcb61452e699faac588c487c78f004fb5fb8
wdwatkins <wwatkins@usgs.gov>
2021-04-23 16:54:45 -0700

    pull fresh flow obs

--- 2_observations/src/munge_flow_dat.R
+++ 2_observations/src/munge_flow_dat.R
@@ -2,11 +2,10 @@
 get_flow_sites <- function(flow_ind, temp_sites_ind, out_ind) {
   flow_dat <- readRDS(sc_retrieve(flow_ind, 'getters.yml')) %>%
     distinct(site_id) %>% pull(site_id)
-
   flow_sites <- paste0('USGS-', flow_dat)
-
   # find sites not in temperature data
   temp_sites <- readRDS(sc_retrieve(temp_sites_ind, 'getters.yml')) %>%
+    as_tibble() %>%
     distinct(site_id) %>% pull(site_id)
 
   flow_missing <- lubridate::setdiff(flow_sites, temp_sites)
