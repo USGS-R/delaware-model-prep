0c25dec8e9c7198864687a800a09ce0fbcd3e337
limnoliver <soliver@usgs.gov>
2021-06-03 14:01:54 -0500

    sb download file

---
+++ 3_predictions/src/download_files.R
@@ -0,0 +1,7 @@
+get_sb_item <- function(sb_id, sb_names, out_ind) {
+  sbtools::authenticate_sb()
+  out_file <- scipiper::as_data_file(out_ind)
+  sbtools::item_file_download(sb_id = sb_id, names = sb_names, destinations = out_file)
+  gd_put(out_ind)
+
+}
